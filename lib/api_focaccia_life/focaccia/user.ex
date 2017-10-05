0cb07e5f271f3fd80be65667827a01003d356589
Matt <matt@lob.com>
2017-10-05 15:16:45 -0700

    feat(db): Create users (#3)

---
+++ lib/api_focaccia_life/focaccia/user.ex
@@ -0,0 +1,21 @@
+defmodule ApiFocacciaLife.Focaccia.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias ApiFocacciaLife.Focaccia.User
+
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :session_token, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :session_token])
+    |> validate_required([:name, :email, :session_token])
+  end
+end
---
+++ lib/api_focaccia_life_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule ApiFocacciaLifeWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use ApiFocacciaLifeWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(ApiFocacciaLifeWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(ApiFocacciaLifeWeb.ErrorView, :"404")
+  end
+end
